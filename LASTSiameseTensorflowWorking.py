f60c5e12e91e0184ceecd12b26ec876981708fa4
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2018-05-04 15:31:07 +0300

    Update LASTSiameseTensorflowWorking.py

--- LASTSiameseTensorflowWorking.py
+++ LASTSiameseTensorflowWorking.py
@@ -1,14 +1,6 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from builtins import input
-
-#import system things
-from tensorflow.examples.tutorials.mnist import input_data # for data
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data # for data
 import numpy as np
-import os
-
 
 class siamese:
 
@@ -61,23 +53,14 @@ class siamese:
         loss = tf.reduce_mean(losses, name="loss")
         return loss
 
-    def loss_with_step(self):
-        margin = 5.0
-        labels_t = self.y_
-        labels_f = tf.subtract(1.0, self.y_, name="1-yi")          # labels_ = !labels;
-        eucd2 = tf.pow(tf.subtract(self.o1, self.o2), 2)
-        eucd2 = tf.reduce_sum(eucd2, 1)
-        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
-        C = tf.constant(margin, name="C")
-        pos = tf.multiply(labels_t, eucd, name="y_x_eucd")
-        neg = tf.multiply(labels_f, tf.maximum(0.0, tf.subtract(C, eucd)), name="Ny_C-eucd")
-        losses = tf.add(pos, neg, name="losses")
-        loss = tf.reduce_mean(losses, name="loss")
-        return loss
 
 # prepare data and tf.session
 mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
-sess = tf.InteractiveSession()
+
+g = tf.Graph() #reset graph
+sess = tf.InteractiveSession(graph=g)
+
+#tf.reset_default_graph() 
 
 # setup siamese network
 siamese = siamese();
@@ -85,20 +68,10 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(siamese.loss)
 saver = tf.train.Saver()
 tf.initialize_all_variables().run()
 
-# if you just want to load a previously trainmodel?
-load = False
-model_ckpt = './model.meta'
-if os.path.isfile(model_ckpt):
-    input_var = None
-    while input_var not in ['yes', 'no']:
-        input_var = input("We found model files. Do you want to load it and continue training [yes/no]?")
-    if input_var == 'yes':
-        load = True
-
 # start training
 if load: saver.restore(sess, './model')
 
-for step in range(50000):
+for step in range(5000):
     batch_x1, batch_y1 = mnist.train.next_batch(128)
     batch_x2, batch_y2 = mnist.train.next_batch(128)
     batch_y = (batch_y1 == batch_y2).astype('float')
@@ -119,4 +92,3 @@ for step in range(50000):
         saver.save(sess, './model')
         embed = siamese.o1.eval({siamese.x1: mnist.test.images})
         embed.tofile('embed.txt')
-
